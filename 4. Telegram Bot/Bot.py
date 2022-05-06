594e7413aecfa509884ff906fb8f03c13468bd6f
Esaco <esaco@uninorte.edu.co>
2022-05-06 15:29:40 -0500

    Primer avance del bot de Telegram

---
+++ 4. Telegram Bot/Bot.py
@@ -0,0 +1,64 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+logger = logging.getLogger()
+
+
+def start(update, context):
+    logger.info("El usuario ha iniciado el bot.")
+    name = update.message.chat.first_name
+    update.message.reply_text(f'Hola, {name}. Bienvenido!')
+    logger.info('Se ha saludado al usuario.')
+
+
+def encode(update, context):
+    logger.info("El usuario ha solicitado codificar.")
+    text = update.message.text.replace('/encode', '').strip()
+    k = text.split(';')[1] 
+    if k:
+        try:
+            cifrado = cifrado_cesar(text.split(';')[0], int(k), 'cifrar')
+            update.message.reply_text(f'Su mensaje cifrado es:\n{cifrado}')
+        except:
+            update.message.reply_text('Lo sentimos, los datos ingresados no son correctos. Por favor, intente nuevamente.')
+    print(text)
+
+
+def decode(update, context):
+    logger.info("El usuario ha solicitado codificar.")
+    text = update.message.text.replace('/decode', '').strip()
+    k = text.split(';')[1] 
+    if k:
+        try:
+            descifrado = cifrado_cesar(text.split(';')[0], int(k), 'descifrar')
+            update.message.reply_text(f'Su mensaje descifrado es:\n{descifrado}')
+        except:
+            update.message.reply_text('Lo sentimos, los datos ingresados no son correctos. Por favor, intente nuevamente.')
+    print(text)
+
+
+def cifrado_cesar(message, key, mode):
+    message    = message.upper()
+    translated = ""
+    LETTERS    = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol)
+            if mode ==   "cifrar":
+                num = num + key
+            elif mode == "descifrar":
+                num = num - key
+
+            if num >= len(LETTERS):
+                num -= len(LETTERS)
+            elif num < 0:
+                num += len(LETTERS)
+
+            translated += LETTERS[num]
+        else:
+            translated += symbol
+    return translated
+
+
---
+++ 4. Telegram Bot/main.py
@@ -0,0 +1,28 @@
+import os
+import Bot as bot
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+
+
+# Cargamos las variables de entorno al entorno de ejecución.
+load_dotenv()
+TOKEN = os.environ.get('TOKEN')
+
+
+def main():
+    # Para conectar nuestra aplicación con el bot.
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher # Manejador de solicitudes.
+
+    # Para procesar las solicitudes del cliente, se neceita manejar los comandos.
+    dp.add_handler(CommandHandler("start", bot.start))
+    dp.add_handler(CommandHandler("encode", bot.encode))
+
+    # Iniciar el bot.
+    updater.start_polling()
+    # Mantener el ciclo de ejecución hasta que ocurra una interrupción.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
